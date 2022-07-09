cdf3dcbd2bee993b1f67ff7c93e21709ae5ecabf
feras <feras.m94.4@gmail.com>
2022-07-09 13:52:13 +0200

    Challenge5: RG and ACR

--- application/main.tf
+++ application/main.tf
@@ -13,3 +13,17 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "challenge5RG"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "lazynerdsACR"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+
+}
